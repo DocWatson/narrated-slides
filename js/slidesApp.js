2ab53463aa1b94c5f7a92bbc22f1053d75212ef0
Beau Watson <bwatson@fingerpaintmarketing.com>
2014-07-02 17:25:26 -0400

    begin App scaffold, adding test images

--- js/slidesApp.js
+++ js/slidesApp.js
@@ -1,9 +1,129 @@
 var narratedSlides = {
+	// variable to track which slide you're on
+	currentSlide: 0,
+	// variable to hold array of slides
+	slides: null,
+	// variable to hold total slides
+	totalSlides: 0,
+
+	/**
+	 * function to disable previous and next buttons based on certain conditions
+	 */
+	checkControls: function(){
+		//at the beginning
+		if (narratedSlides.currentSlide === 0) {
+			// TODO: disable the prev button
+		} else {
+			// TODO: un-disable the prev button
+		}
+
+		//at the end
+		if (narratedSlides.currentSlide === narratedSlides.totalSlides) {
+			// TODO: disable the next button
+		} else {
+			// TODO: un-disable the next button
+		}
+	},
+
+	/**
+	 * function to run when a slide-viewer is detected
+	 * @return {Void}
+	 */
 	init: function() {
-		console.log("App Initiated");
+		//load the slides
+		narratedSlides.getSlides();
+
+		//set up the launcher button
+		$('.slide-viewer').on(
+			'click',
+			'.launch-slides',
+			function() {
+				// TODO: launch the viewer logic
+			}
+		);
+	},
+
+	/**
+	 * function to load the slide information from the JSON file
+	 */
+	getSlides: function() {
+		// make a JSON request for the slides
+		$.getJSON('slides/slides.json', function(json) {
+			// set the return to the slides variable
+		  narratedSlides.slides = json;
+			narratedSlides.totalSlides = narratedSlides.slides.slideDeck.length;
+			// after getting the json, run the setup function to build nav controls
+			narratedSlides.setupControls();
+		});
+	},
+
+	/**
+	 * function to load in a specified slide
+	 * @param {Integer} targetSlide - the index of the slide to load from the
+	 * array
+	 */
+	loadSlide: function(targetSlide) {
+		narratedSlides.currentSlide = targetSlide;
+		console.log(narratedSlides.currentSlide);
+	},
+
+	/**
+	 * function to easily load the next slide in the deck
+	 */
+	loadNextSlide: function(){
+		narratedSlides.loadSlide(narratedSlides.currentSlide + 1);
+	},
+
+	/**
+	 * function to easily load the previous slide in the deck
+	 */
+	loadPrevSlide: function() {
+		narratedSlides.loadSlide(narratedSlides.currentSlide - 1);
+	},
+
+	/**
+	 * function to set up the slide viewer's controls
+	 */
+	setupControls: function() {
+		//set up the pips
+		narratedSlides.setupSlidePips();
+
+		//set up the next button
+		$('.slide-viewer').on(
+			'click',
+			'.slide-viewer-next',
+			function(e) {
+				e.preventDefault();
+				if(!$(this).hasClass('disabled')){
+					narratedSlides.loadNextSlide();
+				}
+			}
+		);
+
+		//set up the prev button
+		$('.slide-viewer').on(
+			'click',
+			'.slide-viewer-prev',
+			function(e) {
+				e.preventDefault();
+				if(!$(this).hasClass('disabled')){
+					narratedSlides.loadPrevSlide();
+				}
+			}
+		);
+	},
+	/**
+	 * function to prepare clickable pips to jump to specific slides
+	 */
+	setupSlidePips: function() {
+		$.each(narratedSlides.slides.slideDeck, function(index, value){
+			// TODO: build the slide nav pips
+		});
 	}
 }
 
 $(document).ready(function(){
-	narratedSlides.init();
+	if($('.slide-viewer').length > 0 ){
+		narratedSlides.init();
+	}
 });
